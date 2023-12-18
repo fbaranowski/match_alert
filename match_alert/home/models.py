8894305aee6309f259e549b4a38890a611aec925
fbaranowski <05filip@gmail.com>
2023-12-18 22:44:03 +0100

    [FEAT] Create dashboard app with models, add season field to League model

--- match_alert/home/models.py
+++ match_alert/home/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class League(models.Model):
     name = models.CharField(max_length=100)
     href_str = models.TextField()
+    season = models.CharField(max_length=7, default="2023/24")
 
     def __str__(self):
         return self.name
--- match_alert/match_alert/settings.py
+++ match_alert/match_alert/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "home.apps.HomeConfig",
     "users.apps.UsersConfig",
+    "dashboard.apps.DashboardConfig",
     "crispy_forms",
     "crispy_bootstrap4",
 ]
