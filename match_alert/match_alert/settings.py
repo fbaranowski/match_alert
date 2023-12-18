c6d4572b5741f49f44e654d862a32231a392ae55
fbaranowski <05filip@gmail.com>
2023-12-18 23:22:14 +0100

    [FIX] Resolve merge conflict v3

--- match_alert/match_alert/settings.py
+++ match_alert/match_alert/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     "home.apps.HomeConfig",
     "users.apps.UsersConfig",
     "dashboard.apps.DashboardConfig",
-    "match_scraper.apps.MatchScraperConfig",
     "crispy_forms",
     "crispy_bootstrap4",
 ]
